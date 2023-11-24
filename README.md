f12fb5111581ba162958858015eebbcb1ffa1c24
Dan Knutson <dan.knutson@gmail.com>
2023-11-24 11:12:28 -0600

    add criterion benchmarks

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Typeshift Solver
 
-This is a solver for the word game [Typeshift](http://www.playtypeshift.com), which you can play at [Puzzmo](https://www.puzzmo.com). It's mostly an exercise in chasing microbenchmarks.
+This is a solver for the word game [Typeshift](http://www.playtypeshift.com), which you can play at [Puzzmo](https://www.puzzmo.com).
+
+It's mostly an exercise in chasing microbenchmarks. I've gotten it down to 100-500 microseconds (warmed up) depending on the size of the puzzle.
--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,19 @@ use std::time::Instant;
 
 use typeshift_solver::Typeshift;
 
-// TODO
-// eliminate all string processing at startup
-// give an optimal solution order based on speed of new letters used
-// optionally rank all minimal solutions based on best optimal ordering
-// add real benchmark testing
-
 fn main() {
     let start = Instant::now();
 
     let input = include_str!("../files/puzzles/2023-11-19.txt");
     let typeshift = Typeshift::new(input);
     let prep_ts = Instant::now();
-    let prep_time = prep_ts.duration_since(start);
 
     let (solution, steps) = typeshift.find_best_solution();
-    let total_time = Instant::now().duration_since(start);
-    let solve_time = Instant::now().duration_since(prep_ts);
+    let end_ts = Instant::now();
+
+    let prep_time = prep_ts.duration_since(start);
+    let total_time = end_ts.duration_since(start);
+    let solve_time = end_ts.duration_since(prep_ts);
     let size = typeshift.size();
 
     dbg!(size, solution, steps, total_time, prep_time, solve_time);
